9259a4159c1d54637c056628336c0df3e3615b3e
Yash Bhayre <yash.bhayre@tcs.com>
2021-11-16 01:46:41 +0530

    bvs voting complete

--- src/components/screens/Voting.js
+++ src/components/screens/Voting.js
@@ -9,12 +9,8 @@ import Showcase from '../../Assets/categories/MainPage_-08.png'
 import category_bg from '../../Assets/backgrounds/CategoriesBg.png'
 import favoutite_bg from '../../Assets/backgrounds/FavouritesBg.png'
 import AOS from 'aos';
-import b1 from '../../Assets/participants/Breaking/Breaking 1.png'
-import b2 from '../../Assets/participants/Breaking/Breaking 2.png'
-import p1 from '../../Assets/participants/Popping/Popping 1.png'
-import p2 from '../../Assets/participants/Popping/Popping 2.png'
-import p3 from '../../Assets/participants/Popping/Popping 3.png'
-import { Drawer, Button, Space, Radio,notification } from 'antd';
+import vote from '../../Assets/participants/vote.png'
+import { Drawer, Button, Space, Radio,notification,Modal } from 'antd';
 import logo from '../../Assets/logo/BVS_Logo.png'
 import axios from 'axios';
 import { Spin } from 'antd';
@@ -22,6 +18,7 @@ const Voting = () => {
 
     const [selectedCategory,setSelectedCategory] = useState(5) 
     const [visible, setVisible] = useState(false);
+    const [isModalVisible,setIsModalVisible] = useState(false)
     const [screenSize, setScreenSize] = useState(window.innerWidth);
     const [showFloatingBtn,setShowFloatingBtn] = useState(false)
     const [allUsers,setAllUsers] = useState([])
@@ -30,30 +27,11 @@ const Voting = () => {
     const [popping,setPopping] = useState([])
     const [rapping,setRapping] = useState([])
     const [showcase,setShowcase] = useState([])
-    const [loading,setLoading] = useState(false)
-    const breakingParticipant = [
-        {'name':'b1','src':b1},
-        {'name':'b2','src':b2},
-        {'name':'b1','src':b1},
-        {'name':'b2','src':b2},
-        {'name':'b1','src':b1},
-        {'name':'b2','src':b2},
-        {'name':'b1','src':b1},
-        {'name':'b2','src':b2},
-        {'name':'b1','src':b1},
-        {'name':'b2','src':b2},
-        {'name':'b1','src':b1},
-        {'name':'b2','src':b2},
-
-    ]
-
-    const poppingParticipant = [
-        {'name':'b1','src':p1},
-        {'name':'b2','src':p2},
-        {'name':'b1','src':p3},
-        {'name':'b2','src':p1},
-
-    ]
+     const [loading,setLoading] = useState(false)
+    const [participantName,setParticipantName] = useState('')
+    const [categoryName,setCategoryName] = useState('')
+    const [votedList,setVotedList] = useState('')
+   
     const showDrawer = () => {
         setVisible(true);
       };
@@ -62,11 +40,104 @@ const Voting = () => {
         setVisible(false);
       };
 
+    const notificationError = (participant) => {
+        notification['error']({
+            message: 'Already Voted',
+            description:
+              `You have already voted for ${participant} in Breaking Category`,
+          });
+    }
+    const setLocalStorage = () => {
+        let votingList = window.localStorage.getItem('votingList');
+        let obj = {}
+        if(votingList){
+            obj = JSON.parse(votingList)
+        }
+        else{
+             obj = {
+                'Breaking':'',
+                'Popping':'',
+                'Graffiti':'',
+                'Rapping':'',
+                'Showcase':''
+            }
+            window.localStorage.setItem('votingList',JSON.stringify(obj))
+            
+        }
+        
+      
+        switch(categoryName) {
+            case 'Breaking' : {
+                if(obj.Breaking==''){
+                 obj.Breaking = participantName
+                 voteService()
+                }
+                else {
+                   notificationError(obj.Breaking)
+                }
+                break;
+            }
+            case 'Popping' : {
+                if(obj.Popping==''){
+                  obj.Popping = participantName
+                  voteService()
+                }
+                else {
+                    notificationError(obj.Popping)
+                 }
+                break;
+            }
+            case 'Graffiti' : {
+                if(obj.Graffiti==''){
+                    obj.Graffiti = participantName
+                    voteService()
+                  }
+                  else {
+                      notificationError(obj.Graffiti)
+                   }
+                  break;
+            }
+            case 'Rapping' : {
+                if(obj.Rapping==''){
+                    obj.Rapping = participantName
+                    voteService()
+                  }
+                  else {
+                      notificationError(obj.Rapping)
+                   }
+                  break;
+            }
+            case 'Showcase' : {
+                if(obj.Showcase==''){
+                    obj.Showcase = participantName
+                    voteService()
+                  }
+                  else {
+                      notificationError(obj.Showcase)
+                   }
+                  break;
+            }
+            default : {
+            }  
+        }
+        console.log("voting list",obj)
+        window.localStorage.setItem('votingList',JSON.stringify(obj))
+    }
     useEffect(()=>{
         AOS.init({
             duration : 1500
           });
-         
+       getAllUsers()
+          
+      },[])
+
+      const getAllUsers = () => {
+        let votingList = window.localStorage.getItem('votingList');
+        let obj = {}
+        if(votingList){
+            obj = JSON.parse(votingList)
+            setVotedList(obj)
+        }
           window.addEventListener('resize',screenWidth)
           setLoading(true)
           axios.post('https://breezervividshuffle.in/api/getAllUsers')
@@ -77,8 +148,47 @@ const Voting = () => {
           .catch(function (error) {
             console.log(error);
           });
-          
-      },[])
+      }
+
+      const showModal = () => {
+        setIsModalVisible(true);
+      };
+    
+      const voteService = () => {
+        axios.post('https://breezervividshuffle.in/api/castVote', {
+            CategoryName: categoryName,
+            ContestantName: participantName
+          })
+        .then(function (response) {
+          console.log(response)
+          notification['success']({
+            message: 'Voted Successfully',
+            description:
+              `You have successfully voted for ${participantName} in ${categoryName} Category`,
+          });
+          setCategoryName('')
+          setParticipantName('')
+          getAllUsers()
+        })
+        .catch(function (error) {
+          console.log(error);
+          notification['error']({
+            message: 'Something Went Wrong',
+            description:
+              `Please try again`,
+          });
+        });
+      }
+      const handleOk = () => {
+        setIsModalVisible(false);
+        setLocalStorage()
+      };
+    
+      const handleCancel = () => {
+        setIsModalVisible(false);
+        setParticipantName('')
+        setCategoryName('')
+      };
 
       useEffect(()=>{
         console.log("users",allUsers)
@@ -118,38 +228,31 @@ const Voting = () => {
         }
      },[screenSize])
     
-    const CastVote = (participantName,category) => {
-        axios.post('https://breezervividshuffle.in/api/castVote', {
-            CategoryName: category,
-            ContestantName: participantName
-          })
-        .then(function (response) {
-          console.log(response)
-          notification['success']({
-            message: 'Voted Successfully',
-            description:
-              `You have successfully voted for ${participantName} in ${category} Category`,
-          });
-        })
-        .catch(function (error) {
-          console.log(error);
-          notification['error']({
-            message: 'Something Went Wrong',
-            description:
-              `Please try again`,
-          });
-        });
+    const CastVote = (participant,category) => {
+
+        setCategoryName(category)
+        setParticipantName(participant)
+        showModal(participant,category)
+
+      
     }
     const GraffitiScreen = () => {
         return(
             <div>
                 <h1>Graffiti</h1>
                 <div className="favourite_container">
+                    
                     {graffiti.map((item,index)=>{
                         return(
+                            <div className="favourite_container_participant" >
                             <img key={index} onClick={()=>CastVote(item.name,'Graffiti')} className="participant_img" alt={"about_image"} src={`https://breezervividshuffle.in/${item.imgPath}`} fluid></img>
+                                {votedList?.Graffiti == item.name &&<div className="voted">
+                                    <img src={vote} fluid />
+                                </div>}
+                            </div>
                         )
                     })}
+                  
                 </div>
             </div>
         )
@@ -161,7 +264,12 @@ const Voting = () => {
                 <div className="favourite_container">
                     {breaking.map((item,index)=>{
                         return(
-                            <img  key={index} onClick={()=>CastVote(item.name,'Breaking')} className="participant_img" alt={"about_image"} src={`https://breezervividshuffle.in/${item.imgPath}`} fluid></img>
+                            <div className="favourite_container_participant" >
+                              <img  key={index} onClick={()=>CastVote(item.name,'Breaking')} className="participant_img" alt={"about_image"} src={`https://breezervividshuffle.in/${item.imgPath}`} fluid></img>
+                                {votedList?.Breaking == item.name &&<div className="voted">
+                                        <img src={vote} fluid />
+                                </div>}
+                            </div>
                         )
                     })}
                 </div>
@@ -175,7 +283,12 @@ const Voting = () => {
                 <div className="favourite_container">
                     {rapping.map((item,index)=>{
                         return(
+                          <div className="favourite_container_participant" >  
                             <img  key={index} onClick={()=>CastVote(item.name,'Rapping')} className="participant_img" alt={"about_image"} src={`https://breezervividshuffle.in/${item.imgPath}`} fluid></img>
+                                {votedList?.Rapping == item.name &&<div className="voted">
+                                        <img src={vote} fluid />
+                                </div>}
+                          </div>
                         )
                     })}
                 </div>
@@ -189,7 +302,12 @@ const Voting = () => {
                 <div className="favourite_container">
                     {showcase.map((item,index)=>{
                         return(
-                            <img  key={index} onClick={()=>CastVote(item.name,'Showcase')} className="participant_img" alt={"about_image"} src={`https://breezervividshuffle.in/${item.imgPath}`} fluid></img>
+                            <div className="favourite_container_participant" >
+                              <img  key={index} onClick={()=>CastVote(item.name,'Showcase')} className="participant_img" alt={"about_image"} src={`https://breezervividshuffle.in/${item.imgPath}`} fluid></img>
+                                {votedList?.Showcase == item.name &&<div className="voted">
+                                        <img src={vote} fluid />
+                                </div>}
+                            </div>
                         )
                     })}
                 </div>
@@ -203,7 +321,12 @@ const Voting = () => {
                 <div className="favourite_container">
                     {popping.map((item,index)=>{
                         return(
-                            <img  key={index} onClick={()=>CastVote(item.name,'Popping')} className="participant_img" alt={"about_image"} src={`https://breezervividshuffle.in/${item.imgPath}`} fluid></img>
+                            <div className="favourite_container_participant" > 
+                             <img  key={index} onClick={()=>CastVote(item.name,'Popping')} className="participant_img" alt={"about_image"} src={`https://breezervividshuffle.in/${item.imgPath}`} fluid></img>
+                                 {votedList?.Popping == item.name &&<div className="voted">
+                                        <img src={vote} fluid />
+                                </div>}
+                             </div>
                         )
                     })}
                 </div>
@@ -219,23 +342,23 @@ const Voting = () => {
                 </div>
                 <div className="voting_category">
                         <div className="voting_category_row_1" >
-                            <div onClick={()=>setSelectedCategory(1)}>
+                            <div onClick={()=>{setSelectedCategory(1);setVisible(false)}}>
                                 <img alt={"about_image"} className={selectedCategory==1?"category_image_selected":"category_image"} src={Graffiti} fluid></img>
                             </div>
-                            <div  onClick={()=>setSelectedCategory(2)}>
+                            <div  onClick={()=>{setSelectedCategory(2);setVisible(false)}}>
                                  <img alt={"about_image"}  className={selectedCategory==2?"category_image_selected":"category_image"} src={Breaking} fluid></img>
                             </div>
                         </div>
                         <div className="voting_category_row_1" >
-                            <div  onClick={()=>setSelectedCategory(3)}>
+                            <div  onClick={()=>{setSelectedCategory(3);setVisible(false)}}>
                                 <img alt={"about_image"}  className={selectedCategory==3?"category_image_selected":"category_image"} src={Rapping} fluid></img>
                             </div>
-                            <div  onClick={()=>setSelectedCategory(4)}>
+                            <div  onClick={()=>{setSelectedCategory(4);setVisible(false)}}>
                                  <img alt={"about_image"}  className={selectedCategory==4?"category_image_selected":"category_image"} src={Popping} fluid></img>
                             </div>
                         </div>
                         <div className="voting_category_row_1" >
-                            <div  onClick={()=>setSelectedCategory(5)} >
+                            <div  onClick={()=>{setSelectedCategory(5);setVisible(false)}} >
                                 <img alt={"about_image"}  className={selectedCategory==5?"category_image_selected":"category_image"} src={Showcase} fluid></img>
                             </div>
                         </div>
@@ -280,6 +403,11 @@ const Voting = () => {
                 <LeftContainer/>
                 </div>
             </Drawer>
+            <Modal title="Confirmation" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                <div>
+                    <span style={{fontSize:22}}>Vote for <span style={{fontWeight:'bold',color:'blue'}}>{participantName}</span> in <span style={{fontWeight:'bold',color:'red'}}>{categoryName}</span> category ?</span>
+                </div>
+            </Modal>
     
            
 		</div>
